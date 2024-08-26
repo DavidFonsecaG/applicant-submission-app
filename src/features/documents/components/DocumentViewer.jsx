29e1e6a28b3b7d2de58fb1ff44dff89609da48cd
David Fonseca <84871546+DavidFonsecaG@users.noreply.github.com>
2024-08-26 03:08:55 +0000

    Adding OnClick functions to DocumentsViewer button

--- src/features/documents/components/DocumentViewer.jsx
+++ src/features/documents/components/DocumentViewer.jsx
@@ -13,7 +13,11 @@ const DocumentViewer = ({setIsToggled, title, type, downloadUrl}) => {
     navigate(`/applications/${applicationId}`)
   };
 
-  const displayCurrentDocument = () => {
+  const handleAccepted = () => {
+    
+  };
+
+  const handleDenied = () => {
     
   };
 
@@ -27,7 +31,7 @@ const DocumentViewer = ({setIsToggled, title, type, downloadUrl}) => {
               <span className="sm:ml-3">
                 <button 
                   type="button" 
-                  onClick={displayCurrentDocument}
+                  onClick={handleAccepted}
                   className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                   <svg className="-ml-0.5 h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd" />
@@ -35,7 +39,10 @@ const DocumentViewer = ({setIsToggled, title, type, downloadUrl}) => {
                 </button>
               </span>
               <span className="sm:ml-3">
-                <button type="button" className="inline-flex items-center rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                <button 
+                  type="button"
+                  onClick={handleDenied}
+                  className="inline-flex items-center rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                   <svg className="-ml-0.5 h-5 w-5" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                   </svg>
