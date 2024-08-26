89e70b9b7e56e14dad8131cbd71195937b7b93f9
David Fonseca <84871546+DavidFonsecaG@users.noreply.github.com>
2024-08-26 03:03:50 +0000

    Update checked button on documentsViewer

--- src/pages/admin/AdminPage.jsx
+++ src/pages/admin/AdminPage.jsx
@@ -4,7 +4,7 @@ const AdminPage = () => {
 
   return (
     <div className="bg-gray-100 dark:bg-gray-900 dark:text-white text-gray-600 flex overflow-hidden text-sm">
-
+      <h1>Admin Page</h1>
     </div>
   );
 };
